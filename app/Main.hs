1edd7dc68e7def17a7cc4b4239b12e50fbb1f00d
gqo <ggmferguson@gmail.com>
2019-02-04 19:47:39 -0500

    Add send/match message test
    Implementation of "Sending Messages" tutorial on Cloud Haskell site

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,33 @@
 module Main where
 
-import Lib
-import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+
+replyBack :: (ProcessId, String) -> Process ()
+replyBack (sender, msg) = send sender msg
+
+logMessage :: String -> Process ()
+logMessage msg = say $ "handling " ++ msg
 
 main :: IO ()
 main = do
     Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
     node <- newLocalNode t initRemoteTable
-    _ <- runProcess node $ do
+    runProcess node $ do
+        echoPid <- spawnLocal $ forever $ do
+            receiveWait [match logMessage, match replyBack]
+
+        say "send some messages!"
+        send echoPid "hello world"
         self <- getSelfPid
-        send self "hello"
-        hello <- expect :: Process String
-        liftIO $ putStrLn hello
-    return()
+        send echoPid (self, "hello world")
+
+        m <- expectTimeout 1000000
+        case m of
+            Nothing -> die "nothing came back!"
+            Just s -> say $ "got " ++ s ++ " back!"
+        
+        liftIO $ threadDelay 2000000
